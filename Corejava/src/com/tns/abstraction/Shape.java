e1bfce62e4203f28c7ec10f4df1b4b918858c4d0
bindu <bindu@DESKTOP-BM24DO4>
2024-03-15 15:48:33 +0530

    first commit

---
+++ Corejava/src/com/tns/abstraction/Shape.java
@@ -0,0 +1,12 @@
+package com.tns.abstraction;
+
+public class Shape {
+     int length = 2; //data members
+     int breadth = 4;
+     
+     void area()//member functions
+     {
+    	 int ans = length*breadth;
+    	 System.out.println("area is"+ans);
+     }
+}
---
+++ Corejava/src/com/tns/abstraction/client.java
@@ -0,0 +1,12 @@
+package com.tns.abstraction;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		Shape ob= new Shape();//creation of object
+		ob.area();
+	}
+
+}
